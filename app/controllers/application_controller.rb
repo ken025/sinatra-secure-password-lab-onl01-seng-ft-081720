ea3eb464b78c4c44cb350242b8832dafeaaeb370
Kenneth Colon <kennethmanuel250@gmail.com>
2020-10-01 03:42:53 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,17 @@ end
 
   post "/login" do
     ##your code here
+     user = User.find_by(:username => params[:username])
+ 
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    
+    redirect "/success"
+  else
+    
+    redirect "/failure"
   end
+end
 
   get "/failure" do
     erb :failure
